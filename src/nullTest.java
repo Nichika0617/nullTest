148c669eb133bb893c7b79aa64c1166c27b46e02
Yuge Nichika <e195765@ie.u-ryukyu.ac.jp>
2019-12-08 15:16:30 +0900

    1st commit

---
+++ src/nullTest.java
@@ -0,0 +1,12 @@
+public class nullTest {
+
+    static void nullTest(String str) {
+        System.out.println(str.length());
+    }
+
+    public static void main(String[] args){
+        String str = null;
+        nullTest(str);
+    }
+}
+
