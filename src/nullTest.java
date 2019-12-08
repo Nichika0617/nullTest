367a356f7f2e42dc0863fa912949b095cba602d7
Yuge Nichika <e195765@ie.u-ryukyu.ac.jp>
2019-12-08 15:50:21 +0900

    2nd commit

--- src/nullTest.java
+++ src/nullTest.java
@@ -1,7 +1,13 @@
 public class nullTest {
 
     static void nullTest(String str) {
+        try{
         System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("エラー(NullPointerException)が発生しました。");
+            System.out.println(e.getMessage()); //
+        }
     }
 
     public static void main(String[] args){
